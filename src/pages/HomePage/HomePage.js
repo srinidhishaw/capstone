f4ab8fce8011c5b51d31ecfcdefde466f4407c5e
Srinidhi Shaw <srinidhishaw@gmail.com>
2023-11-26 16:14:39 -0500

    make a circle animation to mimic box breathing for the hero

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 // import Header from "../../components/Header/Header"
 import MyForm from "../../components/Form/Form"
 import "./HomePage.scss"
+import Hero from "../../components/Hero/Hero"
 import Header from "../../components/Header/Header"
 function HomePage(){
 return(
 <>
             <Header />
+            <Hero/>
             <MyForm
             />
             </>
